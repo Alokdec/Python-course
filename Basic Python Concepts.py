bafc368c3d8af5cbbf0cbc919e30da2adcd28a80
Alokdec <alok913535@gmail.com>
2025-06-09 23:06:28 +0530

    Add files via upload

---
+++ Basic Python Concepts.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the first number  "  ))
+b=int(input("Enter the second number " ))
+
+c=(a+b)
+d=(a-b)
+e=(a*b)
+f=(a/b)
+
+print("Addition : \t\t\t\t\t ", c )
+print("Subtraction : \t\t\t\t ", d)
+print("Multiplication : \t\t\t ", e)
+print("Division : \t\t\t\t\t ", f)
+
---
+++ Personalized Greeting.py
@@ -0,0 +1,3 @@
+a=(input("Enter the first Name  "  ))
+b=(input("Enter the second Name " ))
+print("Hello,",a,bS, "welcom to python program.")
